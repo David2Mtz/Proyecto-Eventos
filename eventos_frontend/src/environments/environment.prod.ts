003dd9da04a395d0736da2f624d4ee845415428c
David2Mtz <ld.martinez.117@gmail.com>
2026-06-19 01:11:15 -0600

    configurando netlify

--- eventos_frontend/src/environments/environment.prod.ts
+++ eventos_frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-app-en-render.onrender.com/api' // Reemplazar con la URL real después del despliegue
+  apiUrl: 'https://zentryeventos.onrender.com/api' // Reemplazar con la URL real después del despliegue
 };
